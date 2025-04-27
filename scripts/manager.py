30fbcb7d2612b1e5788305a0a015f131d283f38c
lily <angelrecovery@icloud.com>
2025-04-26 22:45:51 -0700

    scripts: manager: Heavy code cleanup

--- scripts/manager.py
+++ scripts/manager.py
@@ -5,49 +5,74 @@ import platform
 from typing import List
 from pathlib import Path
 
-CONFIG_DIR = Path(str(os.getenv("APPDATA")) if platform.system() == "Windows" else
-    os.path.join(str(os.getenv("HOME")), ".config"))
+on_windows: bool = platform.system() == "Windows"
+config_directory = Path(
+    str(os.getenv("APPDATA")) if on_windows else str(os.getenv("HOME"))
+)
+
 
 class Config:
-    def __init__(self, name: str, path: Path):
+    name: str
+    repo_path: Path  # Path to the config directory in the repository itself.
+    actual_path: Path  # Actual path that the config will be pushed to/pulled from.
+
+    def __init__(self, name: str, repo_path: Path):
         self.name = name.lower()
-        self.path = path
+        self.repo_path = repo_path
+
+        self.actual_path = config_directory / self.name
 
     def push(self) -> None:
-        dest = CONFIG_DIR / self.name
         try:
-            shutil.copytree(self.path, dest, dirs_exist_ok=True)
-            print(f"(cfg) Pushed '{self.name}' configuration to '{dest}'")
+            shutil.copytree(self.repo_path, self.actual_path, dirs_exist_ok=True)
+            print(f"(cfg) Pushed '{self.name}' configuration to '{self.actual_path}'")
         except Exception as e:
-            print(f"(cfg) Failed to push '{self.name}' configuration to '{dest}': {e}")
+            print(
+                f"(cfg) Failed to push '{self.name}' configuration to '{self.actual_path}': {e}"
+            )
 
     def pull(self) -> None:
-        src = CONFIG_DIR / self.name
         try:
-            shutil.copytree(src, self.path, dirs_exist_ok=True)
-            print(f"(cfg) Pulled '{self.name}' configuration from '{src}'")
+            shutil.copytree(self.actual_path, self.repo_path, dirs_exist_ok=True)
+            print(f"(cfg) Pulled '{self.name}' configuration from '{self.actual_path}'")
         except Exception as e:
-            print(f"(cfg) Failed to pull '{self.name}' configuration from '{src}': {e}")
+            print(
+                f"(cfg) Failed to pull '{self.name}' configuration from '{self.actual_path}': {e}"
+            )
 
-class ConfigManager:
+
+class Sync:
     configs: List[Config]
-    
+
     def __init__(self):
-        self.configs = []
+        self._collect()
 
-    def collect(self) -> None:
+    def _collect(self) -> None:
         self.configs = [
-            Config(path.name, path) for path in Path("cfg").iterdir() if path.is_dir()]
+            Config(path.name, path) for path in Path("cfg").iterdir() if path.is_dir()
+        ]
 
-    def _confirm(self, action: str) -> bool:
-        return input(f"(cfg) Type 'y' to confirm action: \'{action}\': ").lower().startswith("y")
+    def _cancel_action_cb(self, action: str) -> None:
+        print(f"(cfg) User canceled action: '{action}'")
+        exit(0)
+
+    def _conf_action(self, action: str) -> bool:
+        try:
+            conf = input(f"(cfg) Type 'y' to confirm action: '{action}': ")
+        except KeyboardInterrupt:
+            self._cancel_action_cb(action)
+        return conf.lower().startswith("y")
 
     def push(self) -> None:
-        if not self._confirm("push"): return
+        action = "push"
+        if not self._conf_action(action):
+            self._cancel_action_cb(action)
         for config in self.configs:
             config.push()
 
     def pull(self) -> None:
-        if not self._confirm("pull"): return
+        action = "pull"
+        if not self._conf_action(action):
+            self._cancel_action_cb(action)
         for config in self.configs:
             config.pull()
