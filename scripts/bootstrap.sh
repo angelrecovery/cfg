04042f88b77ed3496a83f30e2830d48b60068482
lily <angelrecovery@icloud.com>
2025-05-26 09:27:39 -0700

    feat: install homebrew with bootstrap script

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,8 +2,14 @@
 
 set -xe
 
+# Install homebrew
+bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# TODO: Install zsh plugins
+# ...
 
 # Install regular software
 brew install \
