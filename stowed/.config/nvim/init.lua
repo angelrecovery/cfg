84d5e86b871d19a96b49cd4430a8f2c74a09a139
lily <angelrecovery@icloud.com>
2025-05-26 01:36:07 -0700

    fix(neovim): disable statusline plugin
    forgot to do this when disabling it earlier

--- stowed/.config/nvim/init.lua
+++ stowed/.config/nvim/init.lua
@@ -165,8 +165,8 @@ require('lazy').setup({
       }
 
       -- -- Statusline
-      local statusline = require 'mini.statusline'
-      statusline.setup { use_icons = vim.g.have_nerd_font }
+      -- local statusline = require 'mini.statusline'
+      -- statusline.setup { use_icons = vim.g.have_nerd_font }
       -- ---@diagnostic disable-next-line: duplicate-set-field
       -- statusline.section_location = function()
       --   return '%2l:%-2v'
